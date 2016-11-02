fafde3bda7594f4de0349ea0a09f3097de3e0ee7
simaski <klomp60@gmail.com>
2016-11-02 02:30:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/administrator/myinstaapp/Constants.java
@@ -0,0 +1,17 @@
+package com.example.administrator.myinstaapp;
+
+/**
+ * Created by Administrator on 3/2/2016.
+ */
+public class Constants {
+
+
+    public static final String BASE_URL = "https://api.instagram.com";
+
+    public static final String CLIENT_ID = "03bde90d48ce471ab3c797b7881999d2";
+    public static final String CLIENT_SECRET = "05bbd915ac444078aa5dedb0331d5321";
+    public static final String REDIRECT_URI = "http://www.rinno.cl/";
+
+    public static final String AUTORISATION_CODE = "authorization_code";
+
+}
